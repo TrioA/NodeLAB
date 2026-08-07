2b5fa587711ee4cc9291b29adeed0f3ec2a67ddf
TrioA <programmer60108@gmail.com>
2026-08-07 11:15:24 +0530

    Added / to /home redirect

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html for the root route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "home", "index.html"));
+    // res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.redirect("home");
 });
 
 app.get("/home", (req, res) => {
